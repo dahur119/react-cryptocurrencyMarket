8828c846222440d18124fbdbf316f1f824239180
dayo <dahurdayo@example.com>
2023-01-17 14:18:04 +0100

    seconf commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import {Layout, Typography, Space} from 'antd'
+import './style/app.css'
+
+
+import {Navbar,Exchanges,HomePage, Cryptocurrency,CryptoDetails, News} from './components'
+import { Link } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
+    <div className="app">
+      <div className="navbar">
+        <Navbar/>
+      </div>
+
+      <div className="main">
+        <Layout>
+
+        <div className="routes">
+        <Routes>
+          <Route path="/homepage" element={<HomePage/>}/>
+          <Route path="/exchanges" element={<Exchanges/>}/>
+          <Route path="/cryptocurrency" element={<Cryptocurrency/>}/>
+          <Route path="/crypto/:coinId" element={<CryptoDetails/>}/>
+          <Route path="/news" element={<News/>}/>
+        </Routes>
+         </div>
+
+
+        </Layout>
+    
+
+        
+    
+
+
+   
+   
+      <div className="footer"  >
+        <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
+          CryptoVerse <br/>
+          All right 
+        </Typography.Title>
+        <Space>
+          <Link to='/'>Home</Link>
+          <Link to='/exchanges'>Exchanges</Link>
+          <Link to='/news'>News</Link>
+          
+           
+
+        </Space>
+        </div>
+
+
+      </div>
+
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
+import 'antd/dist/antd.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>,document.getElementById('root'));
